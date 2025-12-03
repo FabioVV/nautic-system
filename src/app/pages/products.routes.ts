ef4d7d3a5f91b511227538c90f4753cb0d0b9eb6
FabioVV <fabio-varela@protonmail.com>
2025-12-03 14:14:18 -0300

    fix acessories incorrect route permission

--- src/app/pages/products.routes.ts
+++ src/app/pages/products.routes.ts
@@ -7,8 +7,8 @@ import { BoatsPage } from './products/boats/boats';
 import { AccessoriesPage } from './products/accessories/accessories';
 
 export default [
-    { path: 'accessories', component: AccessoriesPage, canActivate: [authGuard], data: { "code": "accessories_types:view" } },
-    { path: 'accessories-types', component: AccessoriesTypesPage, canActivate: [authGuard], data: { "code": "accessories:view" } },
+    { path: 'accessories', component: AccessoriesPage, canActivate: [authGuard], data: { "code": "accessories:view" } },
+    { path: 'accessories-types', component: AccessoriesTypesPage, canActivate: [authGuard], data: { "code": "accessories_types:view" } },
     { path: 'boats', component: BoatsPage, canActivate: [authGuard], data: { "code": "pboats:view" } },
     { path: 'engines', component: EnginesPage, canActivate: [authGuard], data: { "code": "engines:view" } },
 ] as Routes;
